37e918d468062b91dec3c9836dfbe7b11b37dc54
Mansur Dewan <mansurdewan26@gmail.com>
2021-12-14 08:47:35 +0530

    mongodb connect

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connect = () => {
-    return mongoose.connect("");
+    return mongoose.connect("mongodb+srv://mento_clone:mento_clone@mento.x2gr5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
 }
 
